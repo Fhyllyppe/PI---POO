eb42b749017947d10b6ca358f1086e617f8816b6
Luis Eduardo Monteiro Filho <138503672+3du4rdo007@users.noreply.github.com>
2024-05-07 13:33:00 -0300

    Update clientes.php

--- Back-end/clientes.php
+++ Back-end/clientes.php
@@ -1,130 +1,220 @@
 <?php
 
-class Clientes{
-    Private $ClienteID; 
-    Private $Nome; 
-    Private $RazaoSocial;
-    Private $CPF; 
-    Private $RG; 
-    Private $DataNascimento; 
-    Private $Sexo; 
-    Private $Email; 
-    Private $Telefone; 
-    Private $Celular; 
-    Private $Rua; 
-    Private $CEP; 
-    Private $CidadeID; 
-    Private $NumeroCNH; 
-    Private $NumeroRegistroCNH; 
-    Private $ValidadeCNH; 
-    Private $CategoriaCNH; 
-    Private $Observacao; 
-    Private $Situacao; 
-    Private $CNPJ; 
-    Private $InscricaoMunicipal; 
-    Private $InscricaoEstadual; 
-    Private $TipoPessoa; 
-    Private $Senha;
+class Clientes {
+    private $ClienteID;
+    private $Nome;
+    private $RazaoSocial;
+    private $CPF;
+    private $RG;
+    private $DataNascimento;
+    private $Sexo;
+    private $Email;
+    private $Telefone;
+    private $Celular;
+    private $Rua;
+    private $CEP;
+    private $CidadeID;
+    private $NumeroCNH;
+    private $NumeroRegistroCNH;
+    private $ValidadeCNH;
+    private $CategoriaCNH;
+    private $Observacao;
+    private $Situacao;
+    private $CNPJ;
+    private $InscricaoMunicipal;
+    private $InscricaoEstadual;
+    private $TipoPessoa;
+    private $Senha;
 
-    public function __construct($ClienteID,$Nome,$RazaoSocial,$CPF,$RG,$DataNascimento,$Sexo,$Email,$Telefone,$Celular,$Rua,$CEP,$CidadeID,$NumeroCNH,$NumeroRegistroCNH,$ValidadeCNH,$CategoriaCNH,$Observacao,$Situacao,$CNPJ,$InscricaoMunicipal,$InscricaoEstadual,$TipoPessoa,$Senha)
-    {
-        $this->ClienteID = $ClienteID;
-        $this->Nome = $Nome;
-        $this->RazaoSocial = $RazaoSocial;
-        $this->CPF = $CPF;
-        $this->RG = $RG;
-        $this->DataNascimento = $DataNascimento;
-        $this->Sexo = $Sexo;
-        $this->Email = $Email;
-        $this->Telefone = $Telefone;
-        $this->Celular = $Celular;
-        $this->Rua = $Rua;
-        $this->CEP = $CEP;
-        $this->CidadeID = $CidadeID;
-        $this->NumeroCNH = $NumeroCNH;
-        $this->NumeroRegistroCNH = $NumeroRegistroCNH;
-        $this->ValidadeCNH = $ValidadeCNH;
-        $this->CategoriaCNH = $CategoriaCNH;
-        $this->Observacao = $Observacao;
-        $this->Situacao = $Situacao;
-        $this->CNPJ = $CNPJ;
-        $this->InscricaoMunicipal = $InscricaoMunicipal;
-        $this->InscricaoEstadual = $InscricaoEstadual;
-        $this->TipoPessoa = $TipoPessoa;
-        $this->Senha = $Senha;
-    }
-
-    public function getCLIENTEID(){
+    public function getCLIENTEID() {
         return $this->ClienteID;
     }
-    public function getNOME(){
+
+    public function setCLIENTEID($ClienteID) {
+        $this->ClienteID = $ClienteID;
+    }
+
+    public function getNOME() {
         return $this->Nome;
     }
-    public function getRAZAOSOCIAL(){
+
+    public function setNOME($Nome) {
+        $this->Nome = $Nome;
+    }
+
+    public function getRAZAOSOCIAL() {
         return $this->RazaoSocial;
     }
-    public function getCpf(){
+
+    public function setRAZAOSOCIAL($RazaoSocial) {
+        $this->RazaoSocial = $RazaoSocial;
+    }
+
+    public function getCPF() {
         return $this->CPF;
     }
-    public function getRg(){
+
+    public function setCPF($CPF) {
+        $this->CPF = $CPF;
+    }
+
+    public function getRG() {
         return $this->RG;
     }
-    public function getDATANASCIMENTO(){
+
+    public function setRG($RG) {
+        $this->RG = $RG;
+    }
+
+    public function getDATANASCIMENTO() {
         return $this->DataNascimento;
     }
-    public function getSEXO(){
+
+    public function setDATANASCIMENTO($DataNascimento) {
+        $this->DataNascimento = $DataNascimento;
+    }
+
+    public function getSEXO() {
         return $this->Sexo;
     }
-    public function getEMAIL(){
+
+    public function setSEXO($Sexo) {
+        $this->Sexo = $Sexo;
+    }
+
+    public function getEMAIL() {
         return $this->Email;
     }
-    public function getTELEFONE(){
+
+    public function setEMAIL($Email) {
+        $this->Email = $Email;
+    }
+
+    public function getTELEFONE() {
         return $this->Telefone;
     }
-    public function getCELULAR(){
+
+    public function setTELEFONE($Telefone) {
+        $this->Telefone = $Telefone;
+    }
+
+    public function getCELULAR() {
         return $this->Celular;
     }
-    public function getRUA(){
+
+    public function setCELULAR($Celular) {
+        $this->Celular = $Celular;
+    }
+
+    public function getRUA() {
         return $this->Rua;
     }
-    public function getCep(){
+
+    public function setRUA($Rua) {
+        $this->Rua = $Rua;
+    }
+
+    public function getCEP() {
         return $this->CEP;
     }
-    public function getCIDADEID(){
+
+    public function setCEP($CEP) {
+        $this->CEP = $CEP;
+    }
+
+    public function getCIDADEID() {
         return $this->CidadeID;
     }
-    public function getNUMEROCNH(){
+
+    public function setCIDADEID($CidadeID) {
+        $this->CidadeID = $CidadeID;
+    }
+
+    public function getNUMEROCNH() {
         return $this->NumeroCNH;
     }
-    public function getNUMEROREGISTROCNH(){
+
+    public function setNUMEROCNH($NumeroCNH) {
+        $this->NumeroCNH = $NumeroCNH;
+    }
+
+    public function getNUMEROREGISTROCNH() {
         return $this->NumeroRegistroCNH;
     }
-    public function getVALIDADECNH(){
+
+    public function setNUMEROREGISTROCNH($NumeroRegistroCNH) {
+        $this->NumeroRegistroCNH = $NumeroRegistroCNH;
+    }
+
+    public function getVALIDADECNH() {
         return $this->ValidadeCNH;
     }
-    public function getCATEGORIACNH(){
+
+    public function setVALIDADECNH($ValidadeCNH) {
+        $this->ValidadeCNH = $ValidadeCNH;
+    }
+
+    public function getCATEGORIACNH() {
         return $this->CategoriaCNH;
     }
-    public function getOBSERVACAO(){
+
+    public function setCATEGORIACNH($CategoriaCNH) {
+        $this->CategoriaCNH = $CategoriaCNH;
+    }
+
+    public function getOBSERVACAO() {
         return $this->Observacao;
     }
-    public function getSITUACAO(){
+
+    public function setOBSERVACAO($Observacao) {
+        $this->Observacao = $Observacao;
+    }
+
+    public function getSITUACAO() {
         return $this->Situacao;
     }
-    public function getCnpj(){
+
+    public function setSITUACAO($Situacao) {
+        $this->Situacao = $Situacao;
+    }
+
+    public function getCNPJ() {
         return $this->CNPJ;
     }
-    public function getINSCRICAOMUNICIPAL(){
+
+    public function setCNPJ($CNPJ) {
+        $this->CNPJ = $CNPJ;
+    }
+
+    public function getINSCRICAOMUNICIPAL() {
         return $this->InscricaoMunicipal;
     }
-    public function getINSCRICAOESTADUAL(){
+
+    public function setINSCRICAOMUNICIPAL($InscricaoMunicipal) {
+        $this->InscricaoMunicipal = $InscricaoMunicipal;
+    }
+
+    public function getINSCRICAOESTADUAL() {
         return $this->InscricaoEstadual;
     }
-    public function getTIPOPESSOA(){
+
+    public function setINSCRICAOESTADUAL($InscricaoEstadual) {
+        $this->InscricaoEstadual = $InscricaoEstadual;
+    }
+
+    public function getTIPOPESSOA() {
         return $this->TipoPessoa;
     }
-    public function getSENHA(){
+
+    public function setTIPOPESSOA($TipoPessoa) {
+        $this->TipoPessoa = $TipoPessoa;
+    }
+
+    public function getSENHA() {
         return $this->Senha;
     }
-    
-}    
+
+    public function setSENHA($Senha) {
+        $this->Senha = $Senha;
+    }
+}
