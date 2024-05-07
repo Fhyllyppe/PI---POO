e8d40eba87b6691a30b5efd6acfab46019ab6161
Luis Eduardo Monteiro Filho <138503672+3du4rdo007@users.noreply.github.com>
2024-05-07 11:26:47 -0300

    Update contas_pagar.php

--- Back-end/contas_pagar.php
+++ Back-end/contas_pagar.php
@@ -1,31 +1,41 @@
 <?php
-class ContasPagar {
-    private $contasaPagarID;
-    private $valor;
-    private $dataVencimento;
-    private $descricao;
+class Adicionais {
+    private $AdicionalID;
+    private $TipoAdicionalID;
+    private $LocacaoDevolucaoID;
+    private $Situacao;
 
-    public function __construct($contasaPagarID,$valor,$dataVencimento,$descricao) {
-        $this->ContasaPagarID = $contasaPagarID;
-        $this->valor = $valor;
-        $this->dataVencimento = $dataVencimento;
-        $this->descricao = $descricao;
+    public function getADICIONALID() {
+        return $this->AdicionalID;
     }
 
-    public function getCONTASAPAGARID() {
-        return $this->contasaPagarID;
+    public function setADICIONALID($AdicionalID) {
+        $this->AdicionalID = $AdicionalID;
     }
 
-    public function getVALOR() {
-        return $this->valor;
+    public function getTIPOADICIONALID() {
+        return $this->TipoAdicionalID;
     }
 
-    public function getDATAVENCIMENTO() {
-        return $this->dataVencimento;
+    public function setTIPOADICIONALID($TipoAdicionalID) {
+        $this->TipoAdicionalID = $TipoAdicionalID;
     }
 
-    public function getDESCRICAO() {
-        return $this->descricao;
+    public function getLOCACAODEVOLUCAOID() {
+        return $this->LocacaoDevolucaoID;
+    }
+
+    public function setLOCACAODEVOLUCAOID($LocacaoDevolucaoID) {
+        $this->LocacaoDevolucaoID = $LocacaoDevolucaoID;
+    }
+
+    public function getSITUACAO() {
+        return $this->Situacao;
+    }
+?>
+
+    public function setSITUACAO($Situacao) {
+        $this->Situacao = $Situacao;
     }
 }
 ?>
