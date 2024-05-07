f7c4f413976de6365cb1e798ace690253ec3d562
Luis Eduardo Monteiro Filho <138503672+3du4rdo007@users.noreply.github.com>
2024-05-07 11:28:17 -0300

    Update adicionais.php

--- Back-end/adicionais.php
+++ Back-end/adicionais.php
@@ -1,28 +1,42 @@
 <?php
 
-class Adicionais{
-    Private $AdicionalID;
-    Private $TipoAdicionalID;
-    Private $LocacaoDevolucaoID;
-    Private $Situacao;
+class Adicionais {
+    private $AdicionalID;
+    private $TipoAdicionalID;
+    private $LocacaoDevolucaoID;
+    private $Situacao;
 
-    public function __construct($AdicionalID,$TipoAdicionalID,$LocacaoDevolucaoID,$Situacao){
-        $this->AdicionalID = $AdicionalID;
-        $this->TipoAdicionalID = $TipoAdicionalID;
-        $this->LocacaoDevolucaoID = $LocacaoDevolucaoID;
-        $this->Situacao = $Situacao;
-    }
-
-    public function getADICIONALID(){
+    public function getADICIONALID() {
         return $this->AdicionalID;
     }
-    public function getTIPOADICIONALID(){
+
+    public function setADICIONALID($AdicionalID) {
+        $this->AdicionalID = $AdicionalID;
+    }
+
+    public function getTIPOADICIONALID() {
         return $this->TipoAdicionalID;
     }
-    public function getLOCACAODEVOLUCAOID(){
+
+    public function setTIPOADICIONALID($TipoAdicionalID) {
+        $this->TipoAdicionalID = $TipoAdicionalID;
+    }
+
+    public function getLOCACAODEVOLUCAOID() {
         return $this->LocacaoDevolucaoID;
     }
-    public function getSITUACAO(){
+
+    public function setLOCACAODEVOLUCAOID($LocacaoDevolucaoID) {
+        $this->LocacaoDevolucaoID = $LocacaoDevolucaoID;
+    }
+
+    public function getSITUACAO() {
         return $this->Situacao;
     }
-} 
+
+    public function setSITUACAO($Situacao) {
+        $this->Situacao = $Situacao;
+    }
+}
+
+?>
