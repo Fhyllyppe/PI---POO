16bd75401edf2ffa541308e63b996f33b4a7410d
Isaac Xavier <42422460+snowjhbr@users.noreply.github.com>
2024-05-07 14:24:19 -0300

    Update contas_receber.php

--- Back-end/contas_receber.php
+++ Back-end/contas_receber.php
@@ -1,33 +1,97 @@
 <?php
 
-class ContasReceber {
-    private $contasaReceberID;
-    private $valor;
-    private $dataVencimento;
-    private $descricao;
+class ContasReceber{
+    Private $ContaReceberID;
+    Private $Valor;
+    Private $DataVencimento;
+    Private $Desconto;
+    Private $Juros;
+    Private $DataPagamento;
+    Private $ValorPago;
+    Private $Situacao;
+    Private $PlanoContaID;
+    Private $ClienteID;
+    Private $TipoCobrancaRecebimentoID;
+    Private $LocacaoDevolucaoID;
+    Private $Observacao;
 
-    public function __construct($contasaReceberID, $valor, $dataVencimento, $descricao) {
-        $this->id = $contasaReceberID;
-        $this->valor = $valor;
-        $this->dataVencimento = $dataVencimento;
-        $this->descricao = $descricao;
+    public function getCONTARECEBERID(){
+        return $this->ContaReceberID;
     }
-
-    public function getCONTASARECEBERID() {
-        return $this->ContasaReceberID;
+    public function setCONTARECEBERID($ContaReceberID){
+        $this->ContaReceberID = $ContaReceberID;
     }
-
-    public function getVALOR() {
-        return $this->valor;
+    public function getVALOR(){
+        return $this->Valor;
     }
-
-    public function getDATAVENCIMENTO() {
-        return $this->dataVencimento;
+    public function setVALOR($Valor){
+        $this->Valor = $Valor;
     }
-
-    public function getDESCRICAO() {
-        return $this->descricao;
+    public function getDATAVENCIMENTO(){
+        return $this->DataVencimento;
     }
-
+    public function setDATAVENCIMENTO($DataVencimento){
+        $this->DataVencimento = $DataVencimento;
+    }    
+    public function getDESCONTO(){
+        return $this->Desconto;
+    }
+    public function setDESCONTO($Desconto){
+        $this->Desconto = $Desconto;
+    }
+    public function getJUROS(){
+        return $this->Juros;
+    }
+    public function setJUROS($Juros){
+        $this->Juros = $Juros;
+    }
+    public function getDATAPAGAMENTO(){
+        return $this->DataPagamento;
+    }
+    public function setDATAPAGAMENTO($DataPagamento){
+        $this->DataPagamento = $DataPagamento;
+    }
+    public function getVALORPAGO(){
+        return $this->ValorPago;
+    }
+    public function setVALORPAGO($ValorPago){
+        $this->ValorPago = $ValorPago;
+    }
+    public function getSITUACAO(){
+        return $this->Situacao;
+    }
+    public function setSITUACAO($Situacao){
+        $this->Situacao = $Situacao;
+    }
+    public function getCLIENTEID(){
+        return $this->ClienteID;
+    }
+    public function setCLIENTEID($ClienteID){
+        $this->ClienteID = $ClienteID;
+    }
+    public function getPLANOCONTAID(){
+        return $this->PlanoContaID;
+    }
+    public function setPLANOCONTAID($PlanoContaID){
+        $this->PlanoContaID = $PlanoContaID;
+    }
+    public function getTIPOCOBRANCARECEBIMENTOID(){
+        return $this->TipoCobrancaRecebimentoID;
+    }
+    public function setTIPOCOBRANCARECEBIMENTOID($TipoCobrancaRecebimentoID){
+        $this->TipoCobrancaRecebimentoID = $TipoCobrancaRecebimentoID;
+    }
+    public function getLOCACAODEVOLUCAOID(){
+        return $this->LocacaoDevolucaoID;
+    }
+    public function setLOCACAODEVOLUCAOID($LocacaoDevolucaoID){
+        $this->LocacaoDevolucaoID = $LocacaoDevolucaoID;
+    }
+    public function getOBSERVACAO(){
+        return $this->Observacao;
+    }
+    public function setOBERVACAO($Observacao){
+        $this->Observacao = $Observacao;
+    }
 }
-?>
+?> 
