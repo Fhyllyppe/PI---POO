e930382c1ae78d98267d211911562f1223bac7c6
Luis Eduardo Monteiro Filho <138503672+3du4rdo007@users.noreply.github.com>
2024-05-07 21:01:52 -0300

    Update grupos.php

---
+++ Back-end/grupos.php
@@ -0,0 +1,39 @@
+<?php
+class Grupos {
+    private $GrupoID;
+    private $Nome;
+    private $Observacao;
+    private $Situacao;
+
+    public function getGRUPOID() {
+        return $this->GrupoID;
+    }
+
+    public function setCARGOID($CargoID) {
+        $this->GrupoID = $GrupoID;
+    }
+
+   public function getNOME(){
+        return $this->Nome;
+    }
+
+   public function setNOME($Nome){
+        $this->Nome = $Nome;
+    }
+
+   public function getOBSERVACAO(){
+        return $this->Nome;
+   }
+
+   public function setOBSERVACAO($Observacao){
+        $this->Observacao = $Observacao;
+   }
+
+   public function getSITUACAO(){
+       return $this->Situacao;
+   }
+
+   public function setSITUACAO($Situacao){
+       $this->Situacao = $Situacao;
+   }
+?>
