4c4da1b7a5cc77572e7acb67dd5f89a1f6aa0e35
Isaac Xavier <42422460+snowjhbr@users.noreply.github.com>
2024-05-08 10:34:01 -0300

    Update veiculos.php

---
+++ Back-end/veiculos.php
@@ -0,0 +1,161 @@
+<?php
+
+class Veiculos{
+    Private $VeiculoID;
+    Private $Proprietario;
+    Private $Renavam;
+    Private $Placa;
+    Private $Chassi;
+    Private $Marca;
+    Private $Modelo;
+    Private $Ano;
+    Private $ValorLocacao;
+    Private $DataAquisicao;
+    Private $Cor;
+    Private $NumeroPortas;
+    Private $Motor;
+    Private $Cambio;
+    Private $Combustivel;
+    Private $NivelTanque;
+    Private $KmAtual;
+    Private $Disponibilidade;
+    Private $FornecedorID;
+    Private $GrupoID;
+    Private $Observacao;
+    Private $Situacao;
+
+    public function getVEICULOID(){
+        return $this->VeiculoID;
+    }
+    public function setVEICULOID($VeiculoID){
+        $this->VeiculoID = $VeiculoID;
+    }
+    public function getPROPRIETARIO(){
+        return $this->Proprietario;
+    }
+    public function setPROPRIETARIO($Proprietario){
+        $this->Proprietario = $Proprietario;
+    }
+    public function getRENAVAM(){
+        return $this->Renavam;
+    }
+    public function setRENAVAM($Renavam){
+        $this->Renavam = $Renavam;
+    }
+    public function getPLACA(){
+        return $this->Placa;
+    }
+    public function setPLACA($Placa){
+        $this->Placa = $Placa;
+    } 
+    public function getCHASSI(){
+        return $this->Chassi;
+    }
+    public function setCHASSI($Chassi){
+        $this->Chassi = $Chassi;
+    } 
+    public function getMARCA(){
+        return $this->Marca;
+    }
+    public function setMARCA($Marca){
+        $this->Marca = $Marca;
+    } 
+    public function getMODELO(){
+        return $this->Modelo;
+    }
+    public function setMODELO($Modelo){
+        $this->Modelo = $Modelo;
+    } 
+    public function getANO(){
+        return $this->Ano;
+    }
+    public function setANO($Ano){
+        $this->Ano = $Ano;
+    } 
+    public function getVALORLOCACAO(){
+        return $this->ValorLocacao;
+    }
+    public function setVALORLOCACAO($ValorLocacao){
+        $this->ValorLocacao = $ValorLocacao;
+    } 
+    public function getDATAAQUISICAO(){
+        return $this->DataAquisicao;
+    }
+    public function setDATAAQUISICAO($DataAquisicao){
+        $this->DataAquisicao = $DataAquisicao;
+    } 
+    public function getCOR(){
+        return $this->Cor;
+    }
+    public function setCOR($Cor){
+        $this->Cor = $Cor;
+    } 
+    public function getNUMEROPORTAS(){
+        return $this->NumeroPortas;
+    }
+    public function setNUMEROPORTAS($NumeroPortas){
+        $this->NumeroPortas = $NumeroPortas;
+    } 
+    public function getMOTOR(){
+        return $this->Motor;
+    }
+    public function setMOTOR($Motor){
+        $this->Motor = $Motor;
+    } 
+    public function getCAMBIO(){
+        return $this->Cambio;
+    }
+    public function setCAMBIO($Cambio){
+        $this->Cambio = $Cambio;
+    } 
+    public function getCOMBUSTIVEL(){
+        return $this->Combustivel;
+    }
+    public function setCOMBUSTIVEL($Combustivel){
+        $this->Combustivel = $Combustivel;
+    } 
+    public function getNIVELTANQUE(){
+        return $this->NivelTanque;
+    }
+    public function setNIVELTANQUE($NivelTanque){
+        $this->NivelTanque = $NivelTanque;
+    } 
+    public function getKMATUAL(){
+        return $this->KmAtual;
+    }
+    public function setKMATUAL($KmAtual){
+        $this->KmAtual = $KmAtual;
+    } 
+    public function getDISPONIBILIDADE(){
+        return $this->Disponibilidade;
+    }
+    public function setDISPONIBILIDADE($Disponibilidade){
+        $this->Disponibilidade = $Disponibilidade;
+    } 
+    public function getFORNECEDORID(){
+        return $this->FornecedorID;
+    }
+    public function setFORNECEDORID($FornecedorID){
+        $this->FornecedorID = $FornecedorID;
+    } 
+    public function getGRUPOID(){
+        return $this->GrupoID;
+    }
+    public function setGRUPOID($GrupoID){
+        $this->GrupoID = $GrupoID;
+    } 
+    public function getOBSERVACAO(){
+        return $this->Observacao;
+    }
+    public function setOBSERVACAO($Observacao){
+        $this->Observacao = $Observacao;
+    } 
+    public function getSITUACAO(){
+        return $this->Situacao;
+    }
+    public function setSITUACAO($Situacao){
+        $this->Situacao = $Situacao;
+    }   
+        
+}
+?> 
