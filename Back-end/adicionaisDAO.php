1f8746ca03d9d303dedb1e1e63e08785c117ee8c
Isaac Xavier <42422460+snowjhbr@users.noreply.github.com>
2024-05-08 21:30:30 -0300

    Update adicionaisDAO.php

--- Back-end/adicionaisDAO.php
+++ Back-end/adicionaisDAO.php
@@ -1,13 +1,15 @@
 <?php
 
+require_once "adicionais.php";
+require_once "conexao.php"; 
+
 class AdicionaisDAO{
     public function create (Adicionais $Adicionais) {
-        $sql = 'INSERT INTO Adicionais (AdicionalID, TipoAdicionalID, LocacaoDevolucaoID, Situacao) VALUES (?,?,?,?)';
+        $sql = 'INSERT INTO Adicionais (TipoAdicionalID, LocacaoDevolucaoID, Situacao) VALUES (?,?,?)';
         $stmt = Conexao::getConn()->prepare($sql);
-        $stmt->bindValue(1, $Adicionais->getADICIONALID());
-        $stmt->bindValue(2, $Adicionais->getTIPOADICIONALID());
-        $stmt->bindValue(3, $Adicionais->getLOCACAODEVOLUCAOID());
-        $stmt->bindValue(4, $Adicionais->getSITUACAO());
+        $stmt->bindValue(1, $Adicionais->getTIPOADICIONALID());
+        $stmt->bindValue(2, $Adicionais->getLOCACAODEVOLUCAOID());
+        $stmt->bindValue(3, $Adicionais->getSITUACAO());
 
         $stmt->execute();
     }
@@ -27,12 +29,12 @@ class AdicionaisDAO{
     }
 
     public function update(Adicionais $Adicionais) {
-        $sql = 'UPDATE Adicionais SET AdicionalID = ?, TipoAdicionalID = ?, LocacaoDevolucaoID = ?, Situacao = ? WHERE AdicionalID = ?';
+        $sql = 'UPDATE Adicionais SET TipoAdicionalID = ?, LocacaoDevolucaoID = ?, Situacao = ? WHERE AdicionalID = ?';
         $stmt = Conexao::getConn()->prepare($sql);
-        $stmt->bindValue(1, $Adicionais->getADICIONALID());
-        $stmt->bindValue(2, $Adicionais->getTIPOADICIONALID());
-        $stmt->bindValue(3, $Adicionais->getLOCACAODEVOLUCAOID());
-        $stmt->bindValue(4, $Adicionais->getSITUACAO());
+        
+        $stmt->bindValue(1, $Adicionais->getTIPOADICIONALID());
+        $stmt->bindValue(2, $Adicionais->getLOCACAODEVOLUCAOID());
+        $stmt->bindValue(3, $Adicionais->getSITUACAO());
 
         $stmt->execute();
     }
@@ -45,4 +47,4 @@ class AdicionaisDAO{
         $stmt->execute();
     }
 }
-?>    
+?>      
