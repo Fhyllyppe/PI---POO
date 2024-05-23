ede7e6a26a219cb78046545c9593566dcbec7080
Isaac Xavier <42422460+snowjhbr@users.noreply.github.com>
2024-05-23 13:42:47 -0300

    Add files via upload
    telas do acesso admin. necessário dar uma melhoradas e fazer alguns ajustes necessários

---
+++ Front-end/admin/contas_a_receber.js
@@ -0,0 +1,16 @@
+function toggleSubMenu(menuId) {
+    const submenu = document.getElementById(menuId);
+    if (submenu.style.display === 'none' || submenu.style.display === '') {
+        submenu.style.display = 'block';
+    } else {
+        submenu.style.display = 'none';
+    }
+}
+
+function openModal() {
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
