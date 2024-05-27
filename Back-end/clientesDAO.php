a171f72e98cc5c131d455f46ddcf7ba1c7d9644f
Isaac Xavier <42422460+snowjhbr@users.noreply.github.com>
2024-05-26 21:24:45 -0300

    Update clientesDAO.php
    Adicionei método para validação de CPF e CNPJ
    
    // Remove qualquer caractere que não seja número
    // Verifica se o CPF tem 11 dígitos
    // Verifica se todos os dígitos são iguais
    // Calcula os dígitos verificadores

--- Back-end/clientesDAO.php
+++ Back-end/clientesDAO.php
@@ -1,7 +1,75 @@
 <?php
 
-class ClientesDAO{
-    public function create (Clientes $Clientes) {
+class ClientesDAO {
+
+    private function validarCPF($cpf) {
+        // Remove qualquer caractere que não seja número
+        $cpf = preg_replace('/[^0-9]/', '', $cpf);
+        
+        // Verifica se o CPF tem 11 dígitos
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        // Calcula os dígitos verificadores
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function validarCNPJ($cnpj) {
+        // Remove qualquer caractere que não seja número
+        $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+
+        // Verifica se o CNPJ tem 14 dígitos
+        if (strlen($cnpj) != 14) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais
+        if (preg_match('/(\d)\1{13}/', $cnpj)) {
+            return false;
+        }
+
+        // Calcula os dígitos verificadores
+        for ($t = 12; $t < 14; $t++) {
+            for ($d = 0, $c = 0, $p = $t - 7; $c < $t; $c++) {
+                $d += $cnpj[$c] * $p--;
+                if ($p < 2) $p = 9;
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cnpj[$c] != $d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function create(Clientes $Clientes) {
+        // Valida CPF
+        if (!$this->validarCPF($Clientes->getCpf())) {
+            throw new Exception("CPF inválido");
+        }
+
+        // Valida CNPJ (se aplicável)
+        if ($Clientes->getCnpj() && !$this->validarCNPJ($Clientes->getCnpj())) {
+            throw new Exception("CNPJ inválido");
+        }
+
         $sql = 'INSERT INTO Clientes (Nome, RazaoSocial, CPF, RG, DataNascimento, Sexo, Email, Telefone, Celular, Rua, CEP, CidadeID, NumeroCNH, NumeroRegistroCNH, ValidadeCNH, CategoriaCNH, Observacao, Situacao, CNPJ, InscricaoMunicipal, InscricaoEstadual, TipoPessoa, Senha) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
         $stmt = Conexao::getConn()->prepare($sql);
         $stmt->bindValue(1, $Clientes->getNOME());
@@ -28,12 +96,10 @@ class ClientesDAO{
         $stmt->bindValue(22, $Clientes->getTIPOPESSOA());
         $stmt->bindValue(23, $Clientes->getSENHA());
 
-        
-
         $stmt->execute();
     }
 
-    public function read(){
+    public function read() {
         $sql = 'SELECT * FROM Clientes';
 
         $stmt = Conexao::getConn()->prepare($sql);
@@ -48,7 +114,7 @@ class ClientesDAO{
     }
 
     public function update(Clientes $Clientes) {
-        $sql = 'UPDATE Clientes SET Nome = ?, RazaoSocial = ?, CPF = ?, RG = ?, DataNascimento = ?, Sexo = ?, Email = ?, Telefone = ?, Celular = ?, Rua = ?, CEP = ?, CidadeID = ?, NumeroCNH = ?, NumeroRegistroCNH = ?, ValidadeCNH = ?, CategoriaCNH = ?, Observacao = ?, Situacao = ?, CNPJ = ?, InscricaoMunicipal = ?, InscricaoEstadual = ?, TipoPessoa = ?, Senha = ?  WHERE ClienteID = ?';
+        $sql = 'UPDATE Clientes SET Nome = ?, RazaoSocial = ?, CPF = ?, RG = ?, DataNascimento = ?, Sexo = ?, Email = ?, Telefone = ?, Celular = ?, Rua = ?, CEP = ?, CidadeID = ?, NumeroCNH = ?, NumeroRegistroCNH = ?, ValidadeCNH = ?, CategoriaCNH = ?, Observacao = ?, Situacao = ?, CNPJ = ?, InscricaoMunicipal = ?, InscricaoEstadual = ?, TipoPessoa = ?, Senha = ? WHERE ClienteID = ?';
         $stmt = Conexao::getConn()->prepare($sql);
         $stmt->bindValue(1, $Clientes->getNOME());
         $stmt->bindValue(2, $Clientes->getRAZAOSOCIAL());
@@ -73,7 +139,7 @@ class ClientesDAO{
         $stmt->bindValue(21, $Clientes->getINSCRICAOESTADUAL());
         $stmt->bindValue(22, $Clientes->getTIPOPESSOA());
         $stmt->bindValue(23, $Clientes->getSENHA());
-        
+        $stmt->bindValue(24, $Clientes->getClienteID());
 
         $stmt->execute();
     }
@@ -86,4 +152,4 @@ class ClientesDAO{
         $stmt->execute();
     }
 }
-?>    
+?>
